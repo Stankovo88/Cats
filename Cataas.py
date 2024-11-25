d70589433832f41880e8016c8765fc8c0577c938
Alex <stankovo88@gmail.com>
2024-11-25 17:03:46 +0300

    Создана кнопка для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -3,20 +3,26 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from bottle import response
 
 
-def load_image():
+
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
         return ImageTk.PhotoImage(img)
-    except Exception as e
+    except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
 
 
 window = Tk()
@@ -25,11 +31,11 @@ window.geometry("600x480")
 
 label = Label()
 label.pack()
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
 url = "https://cataas.com/cat"
-img = load_image(url)
+set_image()
 
-if img:
-    label.config(image=img)
-    label.image = img
+window.mainloop()
 
